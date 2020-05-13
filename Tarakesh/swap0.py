9aef9a65b14808533e89800933ed1d1cc8858830
Fa1con_GRB <fa1con_grb@storms-MacBook-Air.local>
2020-05-13 10:43:55 +0530

    Added swap file for tarakesh

---
+++ Tarakesh/swap0.py
@@ -0,0 +1,17 @@
+# method1
+# swap
+x,y = [int(inp) for inp in input("input for method 1: ").split()]
+
+temp_var = x
+x = y
+y = temp_var
+print(x, y)
+
+# method2 - without using extra variable
+# swap
+x,y = [int(inp) for inp in input("input for method 1: ").split()]
+
+x = x+y
+y = x-y
+x = x-y
+print(x, y)
